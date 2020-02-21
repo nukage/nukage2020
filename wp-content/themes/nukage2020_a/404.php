3eacffce7d8c8989fc3db57f9de68a8dc7308560
Tom Myruski <thomas.myruski@gmail.com>
2020-02-21 12:05:42 -0500

    404 Page Created

---
+++ wp-content/themes/nukage2020_a/404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="container m-auto">
+    <div class="  h-full mt-64">
+        <div class="  text-center justify-center   ">
+            <div style="font-size:8em" class=" text-center oswald text-risered">
+                <?php _e( '404', 'nukage2020_a' ); ?>
+            </div>
+            <div class=" text-3xl uppercase pb-4  font-thin oswald text-center block">
+                <?php _e( 'Page Not Found', 'nukage2020_a' ); ?>
+            </div>
+            <a href="/" class=" font-thin uppercase oswald  text-center underline text-risered hover:no-underline trans"><?php _e( 'Return Home', 'nukage2020_a' ); ?></a>
+        </div>
+    </div>
+</div>                
+
+<?php get_footer(); ?>
